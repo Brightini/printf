e9ed57d08e56007d190f9dd3bbf8f89ed1a02449
rexfada <rexfada@gmail.com>
2023-01-22 11:07:50 +0100

    added prototypes and stdio.h to the main.h file

--- main.h
+++ main.h
@@ -1,8 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
 #include <stdarg.h>
 
+void print_number(int n);
+void _puts(char *str);
 int _putchar(char c);
 int _printf(const char *format, ...);
 
